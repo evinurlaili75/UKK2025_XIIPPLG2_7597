1e131f669b7f61f078d9c0dfe07588816b072a0a
evinurlaili75 <evinurlailii75@gmail.com>
2025-02-17 14:03:53 +0700

    progres hari 1
    ya progres

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+    <title>Website Galeri Foto</title>
+    
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+       
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-success m-1">Masuk</a> 
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <img src="" class="card-img-top" title="" style="height:12rem;">
+                <div class="card-footer text-center">
+                    <a href="">10 Suka</a>
+                    <a href="">3 Komentar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ koneksi.php
@@ -0,0 +1,3 @@
+<?php
+session_start();
+$koneksi = mysqli_connect('localhoust','root','','UKK2025_XIIPPLG2_7597');
